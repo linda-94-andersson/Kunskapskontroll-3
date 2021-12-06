4a3b7d24470ca10b28f0c285c1cbd7374e91db4e
Linda A <linda.29.andersson@gmail.com>
2021-12-06 21:46:02 +0100

    Toggle for one or two players

--- Script/script.js
+++ Script/script.js
@@ -7,34 +7,52 @@ document.addEventListener("click", (event) => {
         let nameFill = document.createElement("input");
         nameFill.placeholder = "Enter your name";
         section.append(nameFill);
-        document.addEventListener("keypress", (event) => {
-            if (event.key === "Enter") {
-                const nameHolder = nameFill.value;
-                const displayName = document.createElement("p");
-                displayName.textContent = `Player ${nameHolder}`;
-                section.append(displayName);
-            }
-        });
+        onePlayer.disabled = true;
+        twoPlayers.disabled = false; 
+        if (onePlayer.disabled = true) {
+            document.addEventListener("keypress", (event) => {
+                if (event.key === "Enter") {
+                    const nameHolder = nameFill.value;
+                    const displayName = document.createElement("p");
+                    displayName.textContent = `Player ${nameHolder}`;
+                    section.append(displayName);
+                    nameFill.remove(); 
+                    if (displayName.value = true){
+                        twoPlayers.disabled = true; 
+                    }
+                }
+            });
+        }
     } else if (event.target === twoPlayers) {
+        
         let nameFillOne = document.createElement("input");
         let nameFillTwo = document.createElement("input");
         nameFillOne.placeholder = "Player 1 name: ";
         nameFillTwo.placeholder = "Player 2 name: ";
         section.append(nameFillOne, nameFillTwo);
-        if (displayNameOne = true) {
+        onePlayer.disabled = false;
+        twoPlayers.disabled = true;
+        if (twoPlayers.disabled = true) {
+            const displayNameOne = document.createElement("p");
+            const displayNameTwo = document.createElement("p");
             document.addEventListener("keypress", (event) => {
                 if (event.key === "Enter") {
                     const nameHolderOne = nameFillOne.value;
-                    const displayNameOne = document.createElement("p");
                     displayNameOne.textContent = `Player 1 ${nameHolderOne}`;
                     section.append(displayNameOne);
-                    if (displayNameTwo = true) {
+                    nameFillOne.disabled = true;
+                    if (displayNameOne.value = true){
+                        onePlayer.disabled = true;             
+                    }
+                    if (nameFillOne.disabled = true) {
+                        nameFillOne.remove(); 
                         document.addEventListener("keypress", (event) => {
                             if (event.key === "Enter") {
                                 const nameHolderTwo = nameFillTwo.value;
-                                const displayNameTwo = document.createElement("p");
                                 displayNameTwo.textContent = `Player 2 ${nameHolderTwo}`;
                                 section.append(displayNameTwo);
+                                nameFillTwo.disabled = true;
+                                nameFillTwo.remove(); 
                             }
                         });
                     }
@@ -43,4 +61,3 @@ document.addEventListener("click", (event) => {
         }
     }
 });
-
